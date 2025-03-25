b93c16070833fac0f7dccfcfed83fa2cec640e9b
alvinulv <alvinulvb0159@gmail.com>
2025-03-25 13:29:59 +0100

    commented code, might be bad, sry

--- Assets/Scripts/cubemovement.cs
+++ Assets/Scripts/cubemovement.cs
@@ -4,7 +4,7 @@ public class cubemovement : MonoBehaviour
 {
     public GameObject othercube;
     public float speed = 1.2f;
-    public bool color;
+    public bool bluecolor;
     public float upperbound = 4.8f;
     public float lowerbound = -3.6f;
     public float rightbound = 6;
@@ -13,7 +13,7 @@ public class cubemovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (color)
+        if (bluecolor)
         {
             othercube = GameObject.FindWithTag("RedCube");
         }
@@ -25,16 +25,23 @@ public class cubemovement : MonoBehaviour
     {
         movementfailed = false;
         //moving left
+        //commenting only this one, others follow the same structure
         if (transform.position.x > leftbound)
         {
+            //player input check
             if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
+                //checks if the other cube is blocking the movement
                 if (transform.position + new Vector3(-speed, 0, 0) != othercube.transform.position)
                 {
+                    //moves the other cube if it got blocked
+                    //basically just a debug, neccesary because one script runs before the other
+                    //so it's possible for a cube to be blocked when it shouldn't be without this
                     if (othercube.GetComponent<cubemovement>().movementfailed)
                     {
                         othercube.transform.position = transform.position;
                     }
+                    //does the actual moving of the cube
                     transform.position = transform.position + new Vector3(-speed, 0, 0);
                 }
                 else movementfailed = true;
@@ -67,7 +74,6 @@ public class cubemovement : MonoBehaviour
                     if (othercube.GetComponent<cubemovement>().movementfailed)
                     {
                         othercube.transform.position = transform.position;
-
                     }
                     transform.position = transform.position + new Vector3(0, -speed, 0);
                 }
@@ -84,7 +90,6 @@ public class cubemovement : MonoBehaviour
                     if (othercube.GetComponent<cubemovement>().movementfailed)
                     {
                         othercube.transform.position = transform.position;
-
                     }
                     transform.position = transform.position + new Vector3(0, speed, 0);
                 }
