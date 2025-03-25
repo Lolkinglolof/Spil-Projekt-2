ad4475514e8f1eff25ab9591de0afd7cb1716cbc
alvinulv <alvinulvb0159@gmail.com>
2025-03-25 11:34:08 +0100

    Updated cube movement script (WIP)

--- Assets/Scripts/cubemovement.cs
+++ Assets/Scripts/cubemovement.cs
@@ -4,6 +4,10 @@ public class cubemovement : MonoBehaviour
 {
     public float speed = 1.2f;
     public bool color;
+    public float upperbound = 4.8f;
+    public float lowerbound = -3.6f;
+    public float rightbound = 6;
+    public float leftbound = -7.2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,19 +17,19 @@ public class cubemovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > leftbound)
         {
             transform.position = transform.position + new Vector3(-speed, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) && transform.position.x < rightbound)
         {
             transform.position = transform.position + new Vector3(speed, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S) && transform.position.y > lowerbound)
         {
             transform.position = transform.position + new Vector3(0, -speed,0);
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && transform.position.y < upperbound)
         {
             transform.position = transform.position + new Vector3(0, speed, 0);
         }
