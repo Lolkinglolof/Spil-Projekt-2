2d28220be6fa738a3d03c7edb66d3c0be6d2be6a
alvinulv <alvinulvb0159@gmail.com>
2025-03-25 14:13:37 +0100

    cleaned up my code so much

--- Assets/Scripts/cubemovement.cs
+++ Assets/Scripts/cubemovement.cs
@@ -25,76 +25,45 @@ public class cubemovement : MonoBehaviour
     {
         movementfailed = false;
         //moving left
-        //commenting only this one, others follow the same structure
-        if (transform.position.x > leftbound)
-        {
-            //player input check
             if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                //checks if the other cube is blocking the movement
-                if (transform.position + new Vector3(-speed, 0, 0) != othercube.transform.position)
-                {
-                    //moves the other cube if it got blocked
-                    //basically just a debug, neccesary because one script runs before the other
-                    //so it's possible for a cube to be blocked when it shouldn't be without this
-                    if (othercube.GetComponent<cubemovement>().movementfailed)
-                    {
-                        othercube.transform.position = transform.position;
-                    }
-                    //does the actual moving of the cube
-                    transform.position = transform.position + new Vector3(-speed, 0, 0);
-                }
-                else movementfailed = true;
+                if (transform.position.x > leftbound)
+                    MoveCube(new Vector3(-speed, 0, 0));
             }
-        }
         //moving right
-        if (transform.position.x < rightbound)
-        {
             if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
-                
-                if (transform.position + new Vector3(speed, 0, 0) != othercube.transform.position)
-                {
-                    if (othercube.GetComponent<cubemovement>().movementfailed)
-                    {
-                        othercube.transform.position = transform.position;
-                    }
-                    transform.position = transform.position + new Vector3(speed, 0, 0);
-                }
-                else movementfailed = true;
+                if (transform.position.x < rightbound)
+                    MoveCube(new Vector3(speed, 0, 0));
             }
-        }
        //moving down
-       if (transform.position.y > lowerbound)
-        {
             if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
             {
-                if (transform.position + new Vector3(0, -speed, 0) != othercube.transform.position)
-                {
-                    if (othercube.GetComponent<cubemovement>().movementfailed)
-                    {
-                        othercube.transform.position = transform.position;
-                    }
-                    transform.position = transform.position + new Vector3(0, -speed, 0);
-                }
-                else movementfailed = true;
+                if (transform.position.y > lowerbound)
+                    MoveCube(new Vector3(0, -speed, 0));
             }
-        }
-        //moving up
-        if (transform.position.y < upperbound)
-        {
+        //moving up            
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
-                if (transform.position + new Vector3(0, speed, 0) != othercube.transform.position)
-                {
-                    if (othercube.GetComponent<cubemovement>().movementfailed)
-                    {
-                        othercube.transform.position = transform.position;
-                    }
-                    transform.position = transform.position + new Vector3(0, speed, 0);
-                }
-                else movementfailed = true;
+                if (transform.position.y < upperbound)
+                    MoveCube(new Vector3(0, speed, 0));
             }
+    }
+    void MoveCube(Vector3 movement)
+    {
+        //checks if the other cube is blocking the movement
+        if (transform.position + movement != othercube.transform.position)
+        {
+            //moves the other cube if it got blocked by this one
+            //basically just a debug, neccesary because one script runs before the other
+            //so it's possible for a cube to be blocked when it shouldn't be without this
+            if (othercube.GetComponent<cubemovement>().movementfailed)
+            {
+                othercube.transform.position = transform.position;
+            }
+            //does the actual moving of the cube
+            transform.position = transform.position + movement;
         }
+        else movementfailed = true;
     }
 }
