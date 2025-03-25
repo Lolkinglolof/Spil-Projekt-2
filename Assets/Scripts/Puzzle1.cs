4106a9a647485f3ba68a6b759b4f348ff76dc4a2
Lolkinglolof <Torben.Thiemer@gmail.com>
2025-03-25 08:59:04 +0100

    comments

--- Assets/Scripts/Puzzle1.cs
+++ Assets/Scripts/Puzzle1.cs
@@ -20,7 +20,7 @@ public class Puzzle1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // sets up the initial board
+        // sets up the initial board, this is for test reasons
         for (int i = 0; i < StartBoard.Length; i++)
         {
             char[] column = new char[8];
@@ -53,7 +53,7 @@ public class Puzzle1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //temporary way to start the puzzle
+        // temporary way to start the puzzle
         if (Input.GetKeyDown(KeyCode.E))
         {
             RestartPuzzle();
