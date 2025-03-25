6e4d3daf447c62f8221e2deebbaaa0a515d365ab
alvinulv <alvinulvb0159@gmail.com>
2025-03-25 11:48:05 +0100

    cubes can now also be moved with arrow keys

--- Assets/Scripts/cubemovement.cs
+++ Assets/Scripts/cubemovement.cs
@@ -17,22 +17,37 @@ public class cubemovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > leftbound)
+        //moving left
+        if (transform.position.x > leftbound)
         {
-            transform.position = transform.position + new Vector3(-speed, 0, 0);
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.position = transform.position + new Vector3(-speed, 0, 0);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.D) && transform.position.x < rightbound)
+        //moving right
+        if (transform.position.x < rightbound)
         {
-            transform.position = transform.position + new Vector3(speed, 0, 0);
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.position = transform.position + new Vector3(speed, 0, 0);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.S) && transform.position.y > lowerbound)
+       //moving down
+       if (transform.position.y > lowerbound)
         {
-            transform.position = transform.position + new Vector3(0, -speed,0);
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.position = transform.position + new Vector3(0, -speed, 0);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.W) && transform.position.y < upperbound)
+        //moving up
+        if (transform.position.y < upperbound)
         {
-            transform.position = transform.position + new Vector3(0, speed, 0);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.position = transform.position + new Vector3(0, speed, 0);
+            }
         }
-
     }
 }
