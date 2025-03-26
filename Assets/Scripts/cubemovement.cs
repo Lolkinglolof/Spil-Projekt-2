7443699a0645921b8d00fe2048a84ed0ce4ec892
alvinulv <alvinulvb0159@gmail.com>
2025-03-26 11:24:35 +0100

    Working walls

--- Assets/Scripts/cubemovement.cs
+++ Assets/Scripts/cubemovement.cs
@@ -1,4 +1,6 @@
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class cubemovement : MonoBehaviour
 {
@@ -10,15 +12,20 @@ public class cubemovement : MonoBehaviour
     public float rightbound = 6;
     public float leftbound = -7.2f;
     public bool movementfailed;
-
+    public LayerMask mask;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (bluecolor)
         {
             othercube = GameObject.FindWithTag("RedCube");
+            mask = LayerMask.GetMask("RedWall");
         }
-        else othercube = GameObject.FindWithTag("BlueCube");
+        else
+        {
+            othercube = GameObject.FindWithTag("BlueCube");
+            mask = LayerMask.GetMask("BlueWall");
+        }
     }
 
     // Update is called once per frame
@@ -46,23 +53,26 @@ public class cubemovement : MonoBehaviour
                     MoveCube(new Vector3(0, speed, 0));
             }
     }
-
     void MoveCube(Vector3 movement)
     {
-        //checks if the other cube is blocking the movement
-        //runs if it didn't
-        if (transform.position + movement != othercube.transform.position)
+        if (!Physics2D.Raycast(transform.position, movement, 1, mask))
         {
-            //moves the other cube if it got blocked by this one
-            //basically just a debug, neccesary because one script runs before the other
-            //so it's possible for a cube to be blocked when it shouldn't be without this
-            if (othercube.GetComponent<cubemovement>().movementfailed)
+            Debug.Log(mask + " detected");
+            //checks if the other cube is blocking the movement
+            //runs if it didn't
+            if (transform.position + movement != othercube.transform.position)
             {
-                othercube.transform.position = transform.position;
+                //moves the other cube if it got blocked by this one
+                //basically just a debug, neccesary because one script runs before the other
+                //so it's possible for a cube to be blocked when it shouldn't be without this
+                if (othercube.GetComponent<cubemovement>().movementfailed)
+                {
+                    othercube.transform.position = transform.position;
+                }
+                //does the actual moving of the cube
+                transform.position = transform.position + movement;
             }
-            //does the actual moving of the cube
-            transform.position = transform.position + movement;
+            else movementfailed = true;
         }
-        else movementfailed = true;
     }
 }
