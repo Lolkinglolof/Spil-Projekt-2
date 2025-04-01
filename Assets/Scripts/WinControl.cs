b2192ed773f248960543e0536c5d32b5be072186
alvinulv <alvinulvb0159@gmail.com>
2025-04-01 09:33:35 +0200

    got win condition working, added new scenes

--- Assets/Scripts/WinControl.cs
+++ Assets/Scripts/WinControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinControl : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class WinControl : MonoBehaviour
     public GameObject bluecube;
     public GameObject redgoal;
     public GameObject bluegoal;
+    public string nextscene;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,9 @@ public class WinControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (redcube.transform.position == redgoal.transform.position && bluecube.transform.position == bluegoal.transform.position)
+        {
+            SceneManager.LoadScene(nextscene);
+        }
     }
 }
