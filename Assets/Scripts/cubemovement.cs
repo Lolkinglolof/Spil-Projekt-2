bcec3cc880514cac9f68c720483a97f83a8b9aab
alvinulv <alvinulvb0159@gmail.com>
2025-04-03 14:07:35 +0200

    created intro level for portals, fixed bugs

--- Assets/Scripts/cubemovement.cs
+++ Assets/Scripts/cubemovement.cs
@@ -66,18 +66,9 @@ public class cubemovement : MonoBehaviour
     }
     void MoveCube(Vector3 movement)
     {
-        //checks if we hit a spike
-        
-        
-        if (!Physics2D.Raycast(transform.position, movement, 1, mask) && !Physics2D.Raycast(transform.position, movement, 1, LayerMask.GetMask("NeutralWall")) && othercube.GetComponent<cubemovement>().DontMove == false)
+        if (othercube.GetComponent<cubemovement>().DontMove == false)
         {
-            if (Physics2D.Raycast(transform.position, movement, 1.2f, spike) || Physics2D.Raycast(transform.position, movement, 1.2f, LayerMask.GetMask("NeutralSpike")))
-            {
-                transform.position = spawn.transform.position;
-                othercube.transform.position = othercube.GetComponent<cubemovement>().spawn.transform.position;
-                DontMove = true;
-            }
-            else if (Physics2D.Raycast(transform.position, movement, 1).collider != null && Physics2D.Raycast(transform.position, movement, 1).collider.gameObject.tag == "Portal")
+            if (Physics2D.Raycast(transform.position, movement, 1).collider != null && Physics2D.Raycast(transform.position, movement, 1).collider.gameObject.tag == "Portal")
             {
                 if (othercube.GetComponent<cubemovement>().movementfailed)
                 {
@@ -86,20 +77,29 @@ public class cubemovement : MonoBehaviour
                 GameObject Portal = Physics2D.Raycast(transform.position, movement, 1).collider.gameObject;
                 Portal.GetComponent<PortalScript>().Teleport(movement, gameObject);
             }
-            else if (transform.position + movement != othercube.transform.position)
+            else if (!Physics2D.Raycast(transform.position, movement, 1, mask) && !Physics2D.Raycast(transform.position, movement, 1, LayerMask.GetMask("NeutralWall")))
             {
-                //moves the other cube if it got blocked by this one
-                //basically just a debug, neccesary because one script runs before the other
-                //so it's possible for a cube to be blocked when it shouldn't be without this
-                if (othercube.GetComponent<cubemovement>().movementfailed)
+                if (Physics2D.Raycast(transform.position, movement, 1.2f, spike) || Physics2D.Raycast(transform.position, movement, 1.2f, LayerMask.GetMask("NeutralSpike")))
                 {
-                    othercube.transform.position = transform.position;
+                    transform.position = spawn.transform.position;
+                    othercube.transform.position = othercube.GetComponent<cubemovement>().spawn.transform.position;
+                    DontMove = true;
                 }
-                //does the actual moving of the cube
-                transform.position = transform.position + movement;
+                else if (transform.position + movement != othercube.transform.position)
+                {
+                    //moves the other cube if it got blocked by this one
+                    //basically just a debug, neccesary because one script runs before the other
+                    //so it's possible for a cube to be blocked when it shouldn't be without this
+                    if (othercube.GetComponent<cubemovement>().movementfailed)
+                    {
+                        othercube.transform.position = transform.position;
+                    }
+                    //does the actual moving of the cube
+                    transform.position = transform.position + movement;
+                }
+                else movementfailed = true;
             }
-            else movementfailed = true;
         }
-        
+        return;
     }
 }
