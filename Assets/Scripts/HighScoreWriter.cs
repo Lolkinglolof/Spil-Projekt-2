ee71ea20d929960d56bb02e8ba32efeb4815bd3b
Lolkinglolof <Torben.Thiemer@gmail.com>
2025-05-07 10:22:21 +0200

    highscore system seems to work

--- Assets/Scripts/HighScoreWriter.cs
+++ Assets/Scripts/HighScoreWriter.cs
@@ -1,21 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.IO;
-using System.Linq;
 using TMPro;
+using UnityEngine;
 
 public class HighScoreWriter : MonoBehaviour
 {
     int HighScoreTime;
     int CarryOverTime;
-    GameObject HighScoreTimeText;
     // Start is called before the first frame update
     void Start()
     {
         HighScoreTime = PlayerPrefs.GetInt("HighScoreTime");
         CarryOverTime = PlayerPrefs.GetInt("CarryOverTime");
-        if (CarryOverTime < HighScoreTime)
+        if (CarryOverTime < HighScoreTime || HighScoreTime == 0)
         {
             HighScoreTime = CarryOverTime;
         }
@@ -26,7 +21,7 @@ public class HighScoreWriter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GameObject.FindWithTag("Stopwatch").GetComponent<TMP_Text>().text = "Last Time: "+PlayerPrefs.GetInt("CarryOverTime").ToString();
+        GameObject.FindWithTag("Stopwatch").GetComponent<TMP_Text>().text = "Last Time: " + PlayerPrefs.GetInt("CarryOverTime").ToString();
         GameObject.FindWithTag("HighScore").GetComponent<TMP_Text>().text = "Best Time: " + PlayerPrefs.GetInt("HighScoreTime").ToString();
     }
 }
