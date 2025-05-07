119c66440d96ebc3f80559a8b9e6192455e27e2f
Lolkinglolof <Torben.Thiemer@gmail.com>
2025-05-07 10:02:22 +0200

    Highscore saver start

--- Assets/Scripts/HighScoreWriter.cs
+++ Assets/Scripts/HighScoreWriter.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
 
 public class HighScoreWriter : MonoBehaviour
 {
+    float HighScoreTime;
+    float CarryOverTime;
+    GameObject HighScoreTimeText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        HighScoreTime = PlayerPrefs.GetInt("HighScoreTime");
+        CarryOverTime = PlayerPrefs.GetInt("CarryOverTime");
+        if (CarryOverTime < HighScoreTime)
+        {
+            HighScoreTime = CarryOverTime;
+        }
+        PlayerPrefs.SetInt("HighScoreTime", HighScoreTime);
+        PlayerPrefs.Save();
     }
 
     // Update is called once per frame
